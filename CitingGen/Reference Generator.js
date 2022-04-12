cd6dd3230bdb6bc8d3e7987af652c5b827de63ca
JustinSadie <37691964+JustinSadie@users.noreply.github.com>
2022-04-12 20:24:32 +0200

    Add files via upload

--- CitingGen/Reference Generator.js
+++ CitingGen/Reference Generator.js
@@ -2,14 +2,17 @@ function reference(){
     var author =  document.getElementById("Author").value;
     var date = document.getElementById("date").value;
     var title = document.getElementById("title").value;
+    var titleStr = title.toString();
+    var titleItal = titleStr.italics();
     var URL = document.getElementById("URL").value;
     var DOA = document.getElementById("DOA").value;
     
-    var Reference = author + ". " + date + ". " + title + ". " + URL + " Date of access: " + DOA + "."
+    var Reference = author + ". " + date+ ". " +  titleItal + ". " + URL + " Date of access: " + DOA + ".";
+    
+    
     console.log("test");
     console.log(author);
     document.getElementById("GenRef").value = Reference;
-
 
 }
 
@@ -19,5 +22,29 @@ function clearTable(){
     document.getElementById("title").value = null;
     document.getElementById("URL").value = null;
     document.getElementById("DOA").value = null;
+    document.getElementById("GenRef").value = null;
 }
+
+
+
+// function btnCopy(){
+    
+//     GenRef.select();
+//     document.execCommand('copy');
+
+
+// }
+
+function btnCopy() {
+    
+    var copyText = document.getElementById("GenRef");
+  
+    copyText.select();
+    
+  
+    navigator.clipboard.writeText(copyText.value);
+  
+
+    // alert("Copied the text: " + copyText.value);
+  }
 
